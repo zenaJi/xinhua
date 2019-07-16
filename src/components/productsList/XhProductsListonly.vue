08c2802f5ca4aa7819a67794063abbb67849b226
sensen2015 <1003133987@qq.com>
2019-07-16 20:31:23 +0800

    cs

--- src/components/productsList/XhProductsListonly.vue
+++ src/components/productsList/XhProductsListonly.vue
@@ -1,96 +1,59 @@
 <template>
-  <!--      仅看有货-->
   <div>
-    <div class="onlybigbigXhProductsList">
-      <div class="onlyXhProductsList">
-        <p>仅看有货</p>
-      </div>
-<!--      语种-->
+  <div class="XhProductsListonly-cs">
+    <div @click="one" :class="{XhProductsListonly_cs_red:one_red==1}">仅看有货</div>
+    <div @click="two" :class="{XhProductsListonly_cs_red:two_red==2}">{{two_title}}&nbsp{{two_ico?'∨':'∧'}}</div>
+    <div @click="three" :class="{XhProductsListonly_cs_red:three_red==3}">{{three_title}}&nbsp{{three_ico?'∨':'∧'}}</div>
+    <div @click="four" :class="{XhProductsListonly_cs_red:four_red==4}">{{four_title}}&nbsp{{four_ico?'∨':'∧'}}</div>
+  </div>
+    <div ref="two_reds" class="XhProductsListonly-two-cs" v-show="box_hidden==2">
+      <div :class="{box_select_red:a.checked}" @click="twoPer(a,b)" v-for="(a,b) in ListTwo[0].nameAndCounts" :key="b"><span>{{a.name}}</span><span>√</span></div>
       <div>
-        <XhProductsList_one></XhProductsList_one>
+        <div @click="twoReset" style="background-color: white;border: whitesmoke 1px solid">重置</div>
+        <div @click="twoApi" style="background-color:#c72f2e;color: white;">确定</div>
       </div>
-      <!--    弹出盒子-->
-      <XhProductsList_two></XhProductsList_two>
+    </div>
+    <div ref="three_reds" class="XhProductsListonly-two-cs" v-show="box_hidden==3">
+      <div :class="{box_select_red:a.checked}" @click="threePer(a,b)" v-for="(a,b) in ListTwo[1].nameAndCounts" :key="b" v-if="ListTwo[1]"><span>{{a.name}}</span><span>√</span></div>
+      <div>
+        <div @click="threeReset" style="background-color: white;border: whitesmoke 1px solid">重置</div>
+        <div @click="twoApi" style="background-color:#c72f2e;color: white;">确定</div>
+      </div>
+    </div>
+    <div ref="four_reds" class="XhProductsListonly-two-cs" v-show="box_hidden==4">
+      <div :class="{box_select_red:a.checked}" @click="fourPer(a,b)" v-for="(a,b) in ListTwo[2].nameAndCounts" :key="b" v-if="ListTwo[2]"><span>{{a.name}}</span><span>√</span></div>
+      <div>
+        <div @click="fourReset" style="background-color: white;border: whitesmoke 1px solid">重置</div>
+        <div @click="twoApi" style="background-color:#c72f2e;color: white;">确定</div>
+      </div>
     </div>
 
-
-
-        <!--        <p>语种  v</p>-->
-
-
-<!--        <div class="InstocksonlyProductsList">-->
-<!--          <van-dropdown-menu active-color="#c62f2e" class="InstockonlyProductsList">-->
-<!--            <van-dropdown-item :title="ListTwo[0].group" ref="item" >-->
-<!--              <van-switch-cell v-model="switch1"  v-for="i in ListTwo[0].nameAndCounts" :title="i.name" />-->
-<!--              <div class="onlyProductsListchian"  >-->
-<!--                <van-button block type="info" @click="onConfirm"  class="ResetsonlyProductsList" >重置</van-button>-->
-<!--                <van-button block type="info" @click="onConfirm" class="ResetonlyProductsList">确认</van-button>-->
-<!--              </div>-->
-<!--            </van-dropdown-item>-->
-<!--          </van-dropdown-menu>-->
-<!--        </div>-->
-        <!--        <p>出版  v</p>-->
-<!--        <div class="InstocksonlyProductsList">-->
-<!--          <van-dropdown-menu active-color="#c62f2e" class="InstockonlyProductsList">-->
-<!--            <van-dropdown-item v-model="value" :options="option" />-->
-<!--          </van-dropdown-menu>-->
-<!--        </div>-->
-        <!--        <p>语言  v</p>-->
-<!--        <div class="InstocksonlyProductsList">-->
-<!--          <van-dropdown-menu active-color="#c62f2e" class="InstockonlyProductsList">-->
-<!--            <van-dropdown-item :title="ListTwo[2].group" ref="item" >-->
-<!--              <van-switch-cell v-model="switch2" v-for="i in ListTwo[2].nameAndCounts" :title="i.name" />-->
-<!--              <div  class="onlyProductsListchian">-->
-<!--                <van-button block type="info" @click="onConfirm"  class="ResetsonlyProductsList" >重置</van-button>-->
-<!--                <van-button block type="info" @click="onConfirm" class="ResetonlyProductsList">确认</van-button>-->
-<!--              </div>-->
-<!--            </van-dropdown-item>-->
-<!--          </van-dropdown-menu>-->
-<!--        </div>-->
-      </div>
+  </div>
 </template>
 
 <script>
-  import $ from 'jquery'
-  import XhProductsList_one from "./XhProductsList_one"
-  import XhProductsList_two from "./XhProductsList_two"
 
     export default {
-        name: "XhProductsListonly",
+      name: "XhProductsListonly",
       props:["ListTwo"],
-      components:{
-        "XhProductsList_one":XhProductsList_one,
-        "XhProductsList_two":XhProductsList_two
-      },
       data() {
         return {
-          // show: true,
-          value1: 0,
-          value2: 0,
-          value3: 0,
-          option1: [
-            { text: '综合', value: 0 },
-            { text: '新品优先', value: 1 },
-          ],
-          option2: [
-            { text: '销量', value: 0 },
-            { text: '从低到高', value: 1 },
-            { text: '从高到低', value: 2 },
-          ],
-          option3: [
-            { text: '价格', value: 0 },
-            { text: '从低到高', value: 1 },
-            { text: '从高到低', value: 2 },
-          ],
-          // show: false,
-          shows:true,
-          value: 0,
-          switch1: false,
-          switch2: false,
+          two_title:this.ListTwo[0].group,
+          three_title:this.ListTwo[1].group,
+          four_title:this.ListTwo[2].group,
+          one_red:0,
+          two_red:0,
+          three_red:0,
+          four_red:0,
           option: [],
-          options: [
-            { text: '仅看有货', value: 0 },
-          ]
+          two_ico:true,
+          three_ico:true,
+          four_ico:true,
+          box_hidden:0, //动画盒子显隐
+          two_lists:this.ListTwo[0].nameAndCounts,
+          three_lists:this.ListTwo[1].nameAndCounts,
+          four_lists:this.ListTwo[2].nameAndCounts
+
         }
       },
       created(){
@@ -105,46 +68,134 @@
           this.option.push(per1)
         })
       },
-      mounted(){
-        this.onlyChange()
-      },
       methods: {
-        onlyChange(){
-          $(".wenthreeXhProductsList").click(function(){
-            $(".shoesXhProductsListbox").stop().slideToggle();
-          });
-          $(".duiXhProductsList").click(function () {
-            $(".duiXhProductsList ").css({"borderBottomColor":" #c62f2e","color":" #c62f2e"})
-            $(".duigouXhProductsList").css("display","block")
-          });
-          $(".offXhProductsList").click(function () {
-            $(".shoesXhProductsListbox").slideToggle();
-            $(".duiXhProductsList").css({"borderBottomColor":" #ccc","color":" #000"})
-            $(".duigouXhProductsList").css("display","none")
-            // $(".XhProductsList").css("opacity","1")
+        one(){
+          if(this.one_red ==1){
+            this.one_red =0
+          }else {
+            this.one_red =1
+            this.$emit("twoApi",{inStock:"1"})
+          }
+          this.box_hidden=0
+        },
+        two(){
+          this.two_ico=!this.two_ico
+         if( this.box_hidden==2){
+           this.box_hidden=0
+         }else {
+           this.box_hidden=2
+         }
+        },
+        three(){
+          this.three_ico=!this.three_ico
+          if( this.box_hidden==3){
+            this.box_hidden=0
+          }else {
+            this.box_hidden=3
+          }
+        },
+        four(){
+          this.four_ico=!this.four_ico
+          if( this.box_hidden==4){
+            this.box_hidden=0
+          }else {
+            this.box_hidden=4
+          }
+        },
+        //点击第二个盒子每一项内容高亮隐藏
+        twoPer(item,index){
+          if(!item.checked){
+            item.checked=1
+            this.$set(this.two_lists,index,item)
+          }else {
+            item.checked=0
+            this.$set(this.two_lists,index,item)
+          }
+        },
+        threePer(item,index){
+          if(!item.checked){
+            item.checked=1
+            this.$set(this.three_lists,index,item)
+          }else {
+            item.checked=0
+            this.$set(this.three_lists,index,item)
+          }
+        },
+        fourPer(item,index){
+          if(!item.checked){
+            item.checked=1
+            this.$set(this.four_lists,index,item)
+          }else {
+            item.checked=0
+            this.$set(this.four_lists,index,item)
+          }
+        },
+        //第二个盒子内容高亮全取消
+        twoReset(){
+          for (var i=0;i<this.two_lists.length;i++){
+            this.two_lists[i].checked=0
+            this.$set(this.two_lists,i,this.two_lists[i])
+          }
+        },
+        threeReset(){
+          for (var i=0;i<this.three_lists.length;i++){
+            this.three_lists[i].checked=0
+            this.$set(this.three_lists,i,this.three_lists[i])
+          }
+        },
+        fourReset(){
+          for (var i=0;i<this.four_lists.length;i++){
+            this.four_lists[i].checked=0
+            this.$set(this.four_lists,i,this.four_lists[i])
+          }
+        },
+        twoApi(){
+          this.box_hidden=0
+          var two_all=[]
+          var three_all=[]
+          var four_all=[]
+          var twoApiSelect = this.$refs.two_reds.getElementsByClassName("box_select_red")
+          var threeApiSelect = this.$refs.three_reds.getElementsByClassName("box_select_red")
+          var fourApiSelect = this.$refs.four_reds.getElementsByClassName("box_select_red")
+          for (var i=0;i<twoApiSelect.length;i++){
+            two_all.push(twoApiSelect[i].children[0].innerHTML)
+          }
+          for (var i=0;i<threeApiSelect.length;i++){
+            three_all.push(threeApiSelect[i].children[0].innerHTML)
+          }
+          for (var i=0;i<fourApiSelect.length;i++){
+            four_all.push(fourApiSelect[i].children[0].innerHTML)
+          }
+          var two_join = two_all.join("_")
+          var three_join = three_all.join("_")
+          var four_join = four_all.join("_")
+          if(twoApiSelect.length){
+            this.two_red=2
+            this.two_title=two_join
+          }else {
+            this.two_red=0
+            this.two_title=this.ListTwo[0].group
+          }
+          if(threeApiSelect.length){
+            this.three_red=3
+            this.three_title=three_join
+          }else {
+            this.three_red=0
+            this.three_title=this.ListTwo[1].group
+          }
+          if(fourApiSelect.length){
+            this.four_red=4
+            this.four_title=four_join
+          }else {
+            this.four_red=0
+            this.four_title=this.ListTwo[2].group
+          }
 
-            $(".qingXhProductsList").click(function () {
-              $(".duiXhProductsList").css({"borderBottomColor":" #ccc","color":" #000"})
-              $(".duigouXhProductsList").css("display","none")
-            })
-          })
-        },
-
-      togglebox:function(){
-          this.boxshow = !this.boxshow;
-        },
-          onlyshow(){
-            this.$refs.only.style.color="#c62f2e"
-            this.$refs.only.style.borderColor="#c62f2e"
-          },
-        showPopup() {
-          this.show = true;
-        },
-        showCount(){
-          this.shows = !this.shows;
-        },
-        onConfirm() {
-          this.$refs.item.toggle();
+          var two_join_all=this.ListTwo[0].group+":"+two_join
+          var three_join_all=this.ListTwo[1].group+":"+three_join
+          var four_join_all=this.ListTwo[2].group+":"+four_join
+          var totall_join=two_join_all+","+three_join_all+","+four_join_all
+          this.$emit("twoApi",{attributes:totall_join})
         }
       },
     }
@@ -152,23 +203,71 @@
 
 <style scoped>
   @import "../../assets/css/XhProductsList.css";
-  /*.fade-enter-active ,.fade-leave-active {*/
-  /*  transition:  .8s ;*/
-  /*  transform: translateY(-5rem);*/
-  /*  opacity: 1;*/
-  /*  margin-top: -2rem;*/
-  /*}*/
-  /*!*{*!*/
-  /*!*  transition: all .8s cubic-bezier(1.0, 0.5, 0.8, 1.0);*!*/
-  /*!*  transform: translateY(-5rem);*!*/
-  /*!*  opacity: 1;*!*/
+  .XhProductsListonly-cs{
+    position: fixed;
+    z-index: 6;
+    top: 1.9rem;
+    display: flex;
+    justify-content: space-around;
+    width: 100vw;
+    background-color:#f7f7f7 ;
+    height: 0.8rem;
+  }
+  .XhProductsListonly-cs>div{
+    background-color: white;
+    text-align: center;
+    width:1.52rem;
+    height: 0.57rem;
+    line-height: 0.57rem;
+    color: #939393;
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+  }
+  .XhProductsListonly-cs>div.XhProductsListonly_cs_red{
+    color: red;
+    border: 1px solid red;
+  }
+  .XhProductsListonly-two-cs{
+    max-height: 10.84rem;
+    width: 100vw;
+    background-color: #ffffff;
+    position: fixed;
+    z-index:100;
+    top: 2.48rem;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+    padding:0 0.3rem 0 0.3rem ;
+    text-align: left;
+    overflow-y: scroll;
+  }
+  .XhProductsListonly-two-cs>div{
+    width: 3.16rem;
+    height: 0.8rem;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: normal;
+    border-bottom: 1px solid #ededed;
+    line-height: 0.8rem;
+    display: flex;
+    justify-content: space-between;
+  }
+  .XhProductsListonly-two-cs>div:last-child{
+    margin-top: 0.2rem;
+    width: 100vw;
+    display: flex;
+    justify-content: space-around;
+    border-bottom: none;
+  }
+  .XhProductsListonly-two-cs>div:last-child>div{
+    width: 3.12rem;
+    height: 0.88rem;
+    text-align: center;
+  }
+  .XhProductsListonly-two-cs>div.box_select_red{
+  color: red;
+  border-bottom:1px solid red;
+}
 
-  /*!*}*!*/
-  /*.fade-enter, .slide-fade-leave-to*/
-  /*  !* .slide-fade-leave-active for below version 2.1.8 *! {*/
-  /*  transform: translateY(5rem);*/
-  /*  margin-top: -2rem;*/
-
-  /*  opacity: 0;*/
-  /*}*/
 </style>
